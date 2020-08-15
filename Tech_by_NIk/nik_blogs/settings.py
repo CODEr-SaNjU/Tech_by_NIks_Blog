24d0533cb21c05f07f48958b5975db725303a044
coder-sanju <sanju2help@gmail.com>
2020-08-16 03:26:32 +0530

    procfile and setting update

--- Tech_by_NIk/nik_blogs/settings.py
+++ Tech_by_NIk/nik_blogs/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+
+
+import django_heroku
 import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +131,7 @@ STATICFILES_DIRS =[
 STATIC_ROOT = os.path.join(BASE_DIR,"live-static","static-root")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+django_heroku.settings(locals())
